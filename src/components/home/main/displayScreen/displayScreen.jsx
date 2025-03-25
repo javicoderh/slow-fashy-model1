6d7b7655f52a41596f5e40c18a22a376eaaad968
javicoderh <justarapperguy@gmail.com>
2025-03-25 18:24:16 -0300

    display screen ajustado con solución robusta para asegurar sincronia

--- src/components/home/main/displayScreen/displayScreen.jsx
+++ src/components/home/main/displayScreen/displayScreen.jsx
@@ -1,45 +1,46 @@
-import { useEffect, useState, useMemo } from 'react'
+import { useEffect, useState } from 'react'
 import './displayScreen.css'
 import img1 from '../../../../assets/img1.avif'
 import img2 from '../../../../assets/img2.avif'
 import img3 from '../../../../assets/img3.avif'
 
 const slides = [
-  {
-    img: img1,
-    text: 'Vístete con tiempo, no con prisa.',
-  },
-  {
-    img: img3,
-    text: 'Cada prenda, una historia que respira.',
-  },
-  {
-    img: img2,
-    text: 'Moda lenta, alma libre.',
-  },
+  { img: img1, text: 'Vístete con tiempo, no con prisa.' },
+  { img: img2, text: 'Cada prenda, una historia que respira.' },
+  { img: img3, text: 'Moda lenta, alma libre.' }
 ]
 
 const DisplayScreen = () => {
-  const [index, setIndex] = useState(0)
+  const [currentSlide, setCurrentSlide] = useState(slides[0])
   const [isVisible, setIsVisible] = useState(true)
 
   useEffect(() => {
     const interval = setInterval(() => {
       setIsVisible(false)
-      setTimeout(() => {
-        setIndex(prev => (prev + 1) % slides.length)
-        setIsVisible(true)
-      }, 300) // tiempo de transición
-    }, 4000)
-    return () => clearInterval(interval)
-  }, [])
 
-  const currentSlide = useMemo(() => slides[index], [index])
+      const nextIndex = (slides.indexOf(currentSlide) + 1) % slides.length
+      const nextSlide = slides[nextIndex]
+      const preloadImg = new Image()
+      preloadImg.src = nextSlide.img
+
+      preloadImg.onload = () => {
+        // Esperamos más tiempo para garantizar sincronización total
+        setTimeout(() => {
+          setCurrentSlide(nextSlide)
+          setTimeout(() => {
+            setIsVisible(true)
+          }, 100) // este delay da tiempo al DOM para establecer la nueva imagen
+        }, 300) // fade-out delay
+      }
+    }, 4000)
+
+    return () => clearInterval(interval)
+  }, [currentSlide])
 
   return (
     <div className="displayScreen">
       <div className={`slide ${isVisible ? 'fade-in' : 'fade-out'}`}>
-        <img src={currentSlide.img} alt={`Slide ${index + 1}`} />
+        <img src={currentSlide.img} alt={currentSlide.text} />
         <div className="caption">{currentSlide.text}</div>
       </div>
     </div>
